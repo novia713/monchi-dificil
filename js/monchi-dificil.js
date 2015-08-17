2e0bd1d126affcf9a24ad0066fe01a5163a55256
Leandro Vázquez Cervantes <leandro@leandro.org>
2015-08-17 08:56:29 +0200

    some minor color changes

--- js/monchi-dificil.js
+++ js/monchi-dificil.js
@@ -1,33 +1,29 @@
 
 
 window.addEventListener('DOMContentLoaded', function() {
-  /*
-  var script = document.createElement('script');
-  script.src = "twemoji/twemoji.min.js";
-  document.body.appendChild(script);
-  console.log(script);
-  */
 
-  console.log("monchi addon 2 started");
+  console.log("monchi addon dificil started");
   var theme = document.getElementById('theme');
   theme.content="orange";
 
 
   /* DIFICULTAD */
+
   document.getElementById("difficulty_container").style.display = "block";
 
-
   /* END DIFICULTAD */
+});
 
 
-/*
+
+/**
+ *   var script = document.createElement('script');
+  script.src = "twemoji/twemoji.min.js";
+  document.body.appendChild(script);
+  console.log(script);
     btn_1.innerHTML = "✊"; //🚹
     btn_1.setAttribute("val","✊"); //♣
     btn_2.innerHTML = "🚹";
     btn_3.innerHTML = "♠"; //✋
     btn_4.innerHTML = "㊗"; //㊙
 */
-});
-
-
-
